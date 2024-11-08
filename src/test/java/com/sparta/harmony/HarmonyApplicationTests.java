59d1707f4e5022cbc9466b94e21faece0d7cbb87
HongChan Yoon <hongchani0324@gmail.com>
2024-11-08 12:42:51 +0900

    init commit

---
+++ src/test/java/com/sparta/harmony/HarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.harmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
