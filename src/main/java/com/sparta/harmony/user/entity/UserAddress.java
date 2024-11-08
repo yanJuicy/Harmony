7ff3dbbdd880961089cb6e34a9f45b01271eb1dd
elliaaa <yunjc536147@gmail.com>
2024-11-08 15:35:26 +0900

    식당,카테고리,유저 엔티티 작성

---
+++ src/main/java/com/sparta/harmony/user/entity/UserAddress.java
@@ -0,0 +1,26 @@
+package com.sparta.harmony.user.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+public class UserAddress {
+    private String addrUUID;
+    private String postcode;
+    private String address;
+    private String detailAddress;
+
+    @Builder
+    public UserAddress(String postcode, String address, String detailAddress) {
+        this.addrUUID = UUID.randomUUID().toString();
+        this.postcode = postcode;
+        this.address = address;
+        this.detailAddress = detailAddress;
+    }
+}
