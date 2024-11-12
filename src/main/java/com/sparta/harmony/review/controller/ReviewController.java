8d4bb58ce47c610c592cbce2f8afb8e47f8f962e
chc <311mikecho@gmail.com>
2024-11-12 15:18:50 +0900

    리뷰

--- src/main/java/com/sparta/harmony/review/controller/ReviewController.java
+++ src/main/java/com/sparta/harmony/review/controller/ReviewController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/reviews")
 @RequiredArgsConstructor
