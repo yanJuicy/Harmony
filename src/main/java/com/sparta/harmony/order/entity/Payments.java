b3b9f85b725281b29d6fb37109a8a93886955969
HongChan Yoon <hongchani0324@gmail.com>
2024-11-13 16:22:42 +0900

    - 엔티티 @DynamicUpdate 적용

--- src/main/java/com/sparta/harmony/order/entity/Payments.java
+++ src/main/java/com/sparta/harmony/order/entity/Payments.java
@@ -5,12 +5,14 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.util.UUID;
 
 @Entity
 @Getter
 @Table(name = "p_payments")
+@DynamicUpdate
 @NoArgsConstructor
 public class Payments extends Timestamped {
 
