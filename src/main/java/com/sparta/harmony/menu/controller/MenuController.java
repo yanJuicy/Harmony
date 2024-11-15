5015d6da236a51d9ac5438fe2c6f8bc39643f27c
yanJuicy <hanjub3@gmail.com>
2024-11-15 09:54:31 +0900

    이미지 업로드

--- src/main/java/com/sparta/harmony/menu/controller/MenuController.java
+++ src/main/java/com/sparta/harmony/menu/controller/MenuController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @RestController
@@ -27,6 +33,25 @@ public class MenuController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(result);
+    }
+
+    @PostMapping("/{storeId}/menus/upload")
+    public String uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            // todo: AWS S3 주소로 바꿔야 됨
+            String currentDirectory = System.getProperty("user.dir");
+            Path directoryPath = Paths.get(currentDirectory + "/src/main/resources/image");
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectories(directoryPath); // Create directory if it does not exist
+            }
+
+            Path filePath = directoryPath.resolve(file.getOriginalFilename());
+            file.transferTo(filePath.toFile()); // Save the file
+
+            return "Image uploaded successfully: " + filePath.toAbsolutePath();
+        } catch (IOException e) {
+            return "Failed to upload image: " + e.getMessage();
+        }
     }
 
 }
