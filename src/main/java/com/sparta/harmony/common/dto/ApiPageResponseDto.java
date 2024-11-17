f436d132c8a7a3d63277be38477f24225051c7c8
HongChan Yoon <hongchani0324@gmail.com>
2024-11-17 23:57:37 +0900

    [refactor]: swagger 적용을 위한 security 수정. - order domain swagger 적용.

--- src/main/java/com/sparta/harmony/common/dto/ApiPageResponseDto.java
+++ src/main/java/com/sparta/harmony/common/dto/ApiPageResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.harmony.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,25 +9,34 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Schema(description = "Api Page 응답 Dto")
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiPageResponseDto<T> {
 
+    @Schema(description = "상태 코드", example = "200")
     private int status;
+
+    @Schema(description = "응답 메시지", example = "주문이 성공적으로 접수되었습니다.")
     private String message;
 
+    @Schema(description = "응답 page", example = "1")
     private int page;
 
+    @Schema(description = "페이지의 사이즈", example = "10")
     private int size;
 
+    @Schema(description = "data의 총 개수", example = "200")
     @JsonProperty("total_elements")
     private long totalElements;
 
+    @Schema(description = "총 페이지", example = "20")
     @JsonProperty("total_page")
     private int totalPage;
 
+    @Schema(description = "응답 Data")
     @JsonProperty("data")
     private List<T> data;
 }
--- src/main/java/com/sparta/harmony/order/controller/PaymentsController.java
+++ src/main/java/com/sparta/harmony/order/controller/PaymentsController.java
@@ -1,14 +1,18 @@
 package com.sparta.harmony.order.controller;
 
-import com.sparta.harmony.common.dto.ApiPageResponseDto;
-import com.sparta.harmony.common.dto.ApiResponseDto;
-import com.sparta.harmony.common.handler.success.SuccessResponseHandler;
+import com.sparta.harmony.common.handler.exception.RestApiException;
 import com.sparta.harmony.order.dto.PaymentsDetailResponseDto;
 import com.sparta.harmony.order.dto.PaymentsResponseDto;
+import com.sparta.harmony.order.dto.PaymentsResponsePageDto;
 import com.sparta.harmony.order.service.PaymentsService;
 import com.sparta.harmony.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,54 +29,62 @@ public class PaymentsController {
     private final PaymentsService paymentsService;
 
     // user 이상 사용 가능
+    @Operation(summary = "결제 내역 조회", description = "결제 내역을 조회할 때 사용하는 API. User/Owner의 경우 자기 자신 결재 내역. manager/master의 경우 전체 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "결제 내역 조회에 성공한 경우", content = @Content(schema = @Schema(implementation = PaymentsResponseDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "결제 내역 조회에 실패했을 경우", content = @Content(schema = @Schema(implementation = RestApiException.class), mediaType = "application/json")),
+    })
     @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_OWNER')  or hasAuthority('ROLE_MANAGER') or hasAuthority('ROLE_MASTER')")
     @GetMapping("/payments")
-    public ResponseEntity<ApiPageResponseDto<PaymentsResponseDto>> getPayments(
+    public ResponseEntity<PaymentsResponsePageDto> getPayments(
             @RequestParam(value = "page", defaultValue = "1") int page,
             @RequestParam(value = "size", defaultValue = "10") int size,
             @RequestParam(value = "sort_by", defaultValue = "createdAt") String sortBy,
             @RequestParam(value = "is_asc", defaultValue = "false") boolean isAsc,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        Page<PaymentsResponseDto> paymentsResponseDto = paymentsService.getPayments(userDetails.getUser(), page - 1, size, sortBy, isAsc);
+        PaymentsResponsePageDto paymentsResponsePageDto = paymentsService.getPayments(userDetails.getUser(), page - 1, size, sortBy, isAsc);
 
-        return new SuccessResponseHandler().handlePageSuccess(
-                HttpStatus.OK,
-                "조회에 성공하였습니다.",
-                paymentsResponseDto
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(paymentsResponsePageDto);
     }
 
+
     // owner 이상만 사용 가능
-    @PreAuthorize("hasAuthority('ROLE_OWNER')  or hasAuthority('ROLE_MANAGER') or hasAuthority('ROLE_MASTER')")
+    @Operation(summary = "특정 가게 결제 내역 조회", description = "특정 가게 결제 내역을 조회할 때 사용하는 API. owner 이상만 사용 가능")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "결제 내역 조회에 성공한 경우", content = @Content(schema = @Schema(implementation = PaymentsResponseDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "결제 내역 조회에 실패했을 경우", content = @Content(schema = @Schema(implementation = RestApiException.class), mediaType = "application/json")),
+    })
+    @Parameter(name = "storeId", description = "가게 ID", example = "cab802e2-d172-4ddb-8e25-d2787122a9cc")
+    @PreAuthorize("hasAuthority('ROLE_OWNER') or hasAuthority('ROLE_MANAGER') or hasAuthority('ROLE_MASTER')")
     @GetMapping("/payments/store/{storeId}")
-    public ResponseEntity<ApiPageResponseDto<PaymentsResponseDto>> getPaymentsByStoreId(
+    public ResponseEntity<PaymentsResponsePageDto> getPaymentsByStoreId(
             @PathVariable UUID storeId,
             @RequestParam(value = "page", defaultValue = "1") int page,
             @RequestParam(value = "size", defaultValue = "10") int size,
             @RequestParam(value = "sort_by", defaultValue = "createdAt") String sortBy,
             @RequestParam(value = "is_asc", defaultValue = "false") boolean isAsc
     ) {
-        Page<PaymentsResponseDto> paymentsResponseDto = paymentsService.getPaymentsByStoreId(storeId, page - 1, size, sortBy, isAsc);
+        PaymentsResponsePageDto paymentsResponsePageDto = paymentsService.getPaymentsByStoreId(storeId, page - 1, size, sortBy, isAsc);
 
-        return new SuccessResponseHandler().handlePageSuccess(
-                HttpStatus.OK,
-                "조회에 성공하였습니다.",
-                paymentsResponseDto
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(paymentsResponsePageDto);
     }
 
     // user 이상 사용 가능
+    @Operation(summary = "결제 내역 상세 조회", description = "결제 내역을 상세 조회할 때 사용하는 API. user/owner의 경우 자신의 내역만, manager/master의 경우 모든 내역에 대해 조회 가능")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "결제 내역 조회에 성공한 경우", content = @Content(schema = @Schema(implementation = PaymentsResponseDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "결제 내역 조회에 실패했을 경우", content = @Content(schema = @Schema(implementation = RestApiException.class), mediaType = "application/json")),
+    })
     @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_OWNER')  or hasAuthority('ROLE_MANAGER') or hasAuthority('ROLE_MASTER')")
     @GetMapping("/payments/{paymentsId}")
-    public ResponseEntity<ApiResponseDto<PaymentsDetailResponseDto>> getPaymentsByPaymentsId(@PathVariable UUID paymentsId,
+    public ResponseEntity<PaymentsDetailResponseDto> getPaymentsByPaymentsId(@PathVariable UUID paymentsId,
                                                                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
         PaymentsDetailResponseDto paymentsDetailResponseDto = paymentsService.getPaymentsByPaymentsId(paymentsId, userDetails.getUser());
 
-        return new SuccessResponseHandler().handleSuccess(
-                HttpStatus.OK,
-                "조회에 성공하였습니다.",
-                paymentsDetailResponseDto
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(paymentsDetailResponseDto);
     }
 }
